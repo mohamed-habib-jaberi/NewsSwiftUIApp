b37d5cc1d0ab236404b660d12df4d0b88c444db6
Mohamed JABERI <mohamed.jaberi@externe.e-i.com>
2021-10-27 16:18:10 +0200

    Initial Commit

---
+++ NewsSwiftUIApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsSwiftUIApp
+//
+//  Created by eidd5180 on 27/10/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsSwiftUIApp/NewsSwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsSwiftUIAppApp.swift
+//  NewsSwiftUIApp
+//
+//  Created by eidd5180 on 27/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct NewsSwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
