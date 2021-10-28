29a7ac6fa0d618ae0c6bc987a77ea813bcebfc29
Mohamed JABERI <mohamed.jaberi@externe.e-i.com>
2021-10-28 14:15:00 +0200

    finalise ArticleListView

--- NewsSwiftUIApp/Views/ArticleListView.swift
+++ NewsSwiftUIApp/Views/ArticleListView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ArticleListView: View {
-    
+
+    let articles: [ Article]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List{
+            ForEach(articles){ article in
+                ArticleRowView(article: article)
+            }
+            .listRowInsets(.init(top: 0, leading: 0, bottom: 0, trailing: 0))
+            .listRowSeparator(.hidden)
+
+        }
+        .listStyle(.plain)
     }
 }
 
 struct ArticleListView_Previews: PreviewProvider {
     static var previews: some View {
-        ArticleListView()
+        ArticleListView(articles: Article.previewData)
     }
 }
